54fdae7a82fe232a0834ce7ac2f6d5cb713baf60
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-09-19 13:25:11 +0600

    complete create a product

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 const app = express();
+import cors from "cors";
 const port = process.env.PORT || 5000;
 import dotenv from "dotenv";
 dotenv.config();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
     res.json({
@@ -25,4 +31,69 @@ async function main() {
         console.log(err);
     }
 }
-main();   
+main();
+
+
+// Schemas
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Boolean,
+        default: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+});
+
+
+// Models
+const Product = model("Product", ProductSchema);
+
+
+
+//product api
+app.post("/products", async (req, res) => {
+    try {
+        const product = req.body;
+        const result = await Product.create(product);
+
+        res.send({
+            success: true,
+            message: "Product created successfully!",
+            data: result,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while creating product.",
+            data: {},
+        });
+    }
+});
+
+
